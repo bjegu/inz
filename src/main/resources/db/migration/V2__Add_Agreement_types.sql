1adbcb0a3c4574a3f7d581e5c40e4b4770c05d3a
Dominika Bieg <dominikabieg@gmail.com>
2020-01-02 17:16:11 +0100

    configure flyway after a Hibernate DDL

---
+++ src/main/resources/db/migration/V2__Add_Agreement_types.sql
@@ -0,0 +1,2 @@
+INSERT INTO `agreement_type` (`id`, `agreement_name`, `is_obligatory`, `is_repeatable`) VALUES (1, 'CAR_INSURANCE', false, true);
+INSERT INTO `agreement_type` (`id`, `agreement_name`, `is_obligatory`, `is_repeatable`) VALUES (2, 'TRAVEL_INSURANCE', false, false);
