7a329f02da48b76baf90d9831a04141fdd78cf07
Dominika Bieg <dominikabieg@gmail.com>
2020-01-04 19:07:19 +0100

    add paging for client

--- src/main/java/ClientsSystem/Rest/ClientController.java
+++ src/main/java/ClientsSystem/Rest/ClientController.java
@@ -2,12 +2,10 @@ package ClientsSystem.Rest;
 
 import ClientsSystem.Domain.Model.Client;
 import ClientsSystem.Domain.Service.ClientService;
-import ClientsSystem.Domain.Service.Specification.ClientSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,37 +15,44 @@ import java.util.UUID;
 @RequestMapping("/api/client")
 public class ClientController {
 
-    public final Integer CLIENT_PAGE_SIZE = 10;
+    public static final Integer CLIENT_PAGE_SIZE = 10;
 
     @Autowired
     private ClientService clientService;
 
     @GetMapping("/")
-    public List<Client> showAll(@RequestParam (required = false, defaultValue = "surname") String sort,
-                                @RequestParam (required = false, defaultValue = "ASC") String order,
-                                @RequestParam(required = false) String search){
-        return clientService.findAll(PageRequest.of(0,CLIENT_PAGE_SIZE, Sort.by(Sort.Direction.fromString(order), sort)), search)
-                .getContent();
+    public Page<Client> showAll(@RequestParam(required = false, defaultValue = "0") Integer page,
+                                @RequestParam(required = false, defaultValue = "surname") String sort,
+                                @RequestParam(required = false, defaultValue = "ASC") String order,
+                                @RequestParam(required = false) String search) {
+        return clientService.findAll(preparePageRequest(page, order, sort), search);
+    }
+
+    private PageRequest preparePageRequest(Integer page, String order, String sort) {
+        if (page == -1) {
+            return PageRequest.of(0, 9999, Sort.by(Sort.Direction.fromString(order), sort));
+        }
+        return PageRequest.of(page, CLIENT_PAGE_SIZE, Sort.by(Sort.Direction.fromString(order), sort));
     }
 
     @GetMapping("/search")
-    public List<Client> showAll(@RequestParam String search){
+    public List<Client> showAll(@RequestParam String search) {
         return clientService.searchClient(search);
     }
 
     @PostMapping("/")
-    public Client addEdit(@RequestBody() Client client){
+    public Client addEdit(@RequestBody() Client client) {
 //        client.getAddress().forEach(address -> address.setClient(client));
         return clientService.save(client);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteCl(@PathVariable()UUID id){
+    public void deleteCl(@PathVariable() UUID id) {
         clientService.deleteById(id);
     }
 
     @GetMapping("/{id}")
-    public Client byId(@PathVariable()UUID id) {
+    public Client byId(@PathVariable() UUID id) {
         return clientService.findById(id);
 
     }
