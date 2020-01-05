136b23fe5f0e77eb74688b53af659e16a8d2b6b6
Dominika Bieg <dominikabieg@gmail.com>
2020-01-05 19:24:38 +0100

    MQTT messages send on user input

--- src/main/java/ClientsSystem/Domain/Service/AgreementService.java
+++ src/main/java/ClientsSystem/Domain/Service/AgreementService.java
@@ -2,6 +2,8 @@ package ClientsSystem.Domain.Service;
 
 import ClientsSystem.Domain.Model.Agreement;
 import ClientsSystem.Domain.Repository.AgreementRepositoryI;
+import ClientsSystem.Infrastructure.notification.NotificationQueues;
+import ClientsSystem.Infrastructure.notification.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +18,9 @@ public class AgreementService {
     @Autowired
     private AgreementRepositoryI agreementRepository;
 
+    @Autowired
+    private NotificationService notificationService;
+
     @Transactional
     public Page<Agreement> findAll(PageRequest of) {
         return agreementRepository.findAll(of);
@@ -23,11 +28,18 @@ public class AgreementService {
 
     @Transactional
     public Agreement save(Agreement agreement) {
-        return agreementRepository.save(agreement);
+        final Agreement response = agreementRepository.save(agreement);
+        notificationService.sendNotification(response.getUuid(),
+                response.getClient().getName() + ' ' + response.getClient().getSurname(),
+                response.getClient().getEmail(),
+                response.getEnd(),
+                NotificationQueues.CLIENT_AGREEMENT);
+        return response;
     }
 
     @Transactional
     public void deleteById(UUID id) {
         agreementRepository.deleteById(id);
+        notificationService.sendNotificationDeactivation(id);
     }
 }
